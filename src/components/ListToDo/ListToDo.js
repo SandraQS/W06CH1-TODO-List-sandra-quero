34933b0762d046112a488741674c7490eb3a30c5
SandraQS <sanquesan@gmail.com>
2021-11-01 19:07:04 +0100

    Use useToDo() at listToDo component

--- src/components/ListToDo/ListToDo.js
+++ src/components/ListToDo/ListToDo.js
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
+import useToDo from "../../hooks/useToDo";
+
 const ListToDo = () => {
+  const { toDo, loadToDo } = useToDo();
+
+  useEffect(() => {
+    loadToDo();
+  }, [loadToDo]);
+
+  console.log(toDo);
   return (
-    <ul>
-      <li>Item1</li>
-      <li>Item2</li>
-      <li>Item3</li>
-    </ul>
+    <div className="form-check">
+      {toDo.map((tasck) => (
+        <div key={tasck.id}>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckDefault"
+          />
+          <label className="form-check-label" htmlFor="flexCheckDefault">
+            {tasck.name}
+          </label>
+        </div>
+      ))}
+    </div>
   );
 };
 export default ListToDo;
