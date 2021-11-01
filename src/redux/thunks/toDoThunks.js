193a1ccfa41635ee20cefa799789dc9c74480f60
SandraQS <sanquesan@gmail.com>
2021-11-01 21:10:44 +0100

    Create funcion create new task

--- src/redux/thunks/toDoThunks.js
+++ src/redux/thunks/toDoThunks.js
@@ -1,8 +1,21 @@
-import { loadAction } from "../actions/actionCreators";
+import { createAction, loadAction } from "../actions/actionCreators";
 
 export const loadActionThunks = () => async (dispatch) => {
   const response = await fetch("https://todo-api-sqs.herokuapp.com/toDo");
   const toDoList = await response.json();
 
   dispatch(loadAction(toDoList));
+};
+
+export const createActionThunks = (toDo) => async (dispatch) => {
+  const response = await fetch("https://todo-api-sqs.herokuapp.com/toDo", {
+    method: "POST",
+    body: JSON.stringify(toDo),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const newTask = await response.json();
+  dispatch(createAction(newTask));
 };
