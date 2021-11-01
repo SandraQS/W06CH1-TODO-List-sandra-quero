ad2377bf33d7c6affdedfff7e60139bfd43037f1
SandraQS <sanquesan@gmail.com>
2021-11-01 16:09:49 +0100

    Created files and folders for redux and components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import Form from "./components/Form/Form";
+import ListToDo from "./components/ListToDo/ListToDo";
 
 function App() {
-  return <h1>Hola {`:)`}</h1>;
+  return (
+    <>
+      <h1>Hola {`:)`}</h1>
+      <Form />
+      <ListToDo />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+const Form = () => {
+  return (
+    <>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
+    </>
+  );
+};
+export default Form;
